05f81fc13f9d86f23eacdc9ca7a6fa1f6b4c3e20
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-03-24 00:45:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -60,7 +60,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  for i in number_of_times do
+  for i in number_of_times.range do
     puts phrase
   end
 end
